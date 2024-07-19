a4abb33568fc8597f1e1d4b3751cd338fc598d24
vansh-frontend <vanshdhalor04>
2024-07-19 19:24:50 +0530

    first

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ window.addEventListener("beforeunload", function (e) {
   e.returnValue = message;
   return message;
 });
+
+// loader js
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  const content = document.getElementById("content");
+
+  setTimeout(() => {
+    preloader.style.display = "none";
+    content.classList.remove("hidden");
+  }, 2000);
+});
